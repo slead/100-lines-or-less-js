444bc11a791380b31249b4ff436716c966e2ed9c
David Wilton <djwilton@gmail.com>
2013-03-19 11:05:08 +0000

    Added knockout and bound to model

--- mapbook/js/app.js
+++ mapbook/js/app.js
@@ -8,5 +8,34 @@ require(["dojo/ready", "esri/map"],
                 zoom: 9
             };
             var map = new esri.Map("mapDiv", options);
+
+            // Class to represent a bookmark
+            function bookmarkEntry(name, extent) {
+                var self = this;
+                self.name = name;
+                self.extent = extent;
+            }
+
+            // Overall view model for this screen, along with initial state
+            function boomarksModelModel() {
+                var self = this;
+
+                // Editable data
+                self.bookmarks = ko.observableArray([
+                    new bookmarkEntry("Overview", map.extent.toJson())
+                ]);
+
+                self.current = ko.observable();
+                
+                self.addBookmark = function () {
+                    self.bookmarks.push(new bookmarkEntry(self.current(), map.extent.toJson()));
+                    self.current(''); //clear the current value
+                    localStorage.setItem("myMapBookmarks", self.bookmarks());
+                    
+                };
+            }
+
+            ko.applyBindings(new boomarksModelModel());
+
         });
     })
