2874543252e0ede8e6bc7df9c8be0732481335d6
David Wilton <djwilton@gmail.com>
2013-03-19 13:10:48 +0000

    Ensured initial extent is created as first bookmark

--- mapbook/js/app.js
+++ mapbook/js/app.js
@@ -2,11 +2,17 @@ require(["dojo/ready", "esri/map"],
     function (ready) {
 
         ready(function () {
+            
             var options = {
                 basemap: "streets", //streets | satellite | hybrid | topo | gray | oceans | national-geographic | osm.
-                center: [-79.40, 43.55],
-                zoom: 9
+                extent: new esri.geometry.Extent({ 
+                    "xmin": -13094934.17795995,
+                    "ymin": 3959049.5650010793,
+                    "xmax": -12826640.20867888,
+                    "ymax": 4053525.7319615935, 
+                    "spatialReference": { "wkid": 102100} })
             };
+            
             var map = new esri.Map("map", options);
 
             // Class to represent a bookmark
@@ -16,12 +22,12 @@ require(["dojo/ready", "esri/map"],
                 self.extent = extent;
             }
 
-            // Overall view model for this screen, along with initial state
+            // View model for bookmark
             function boomarksModelModel() {
                 var self = this;
 
                 var bookmarks = JSON.parse(localStorage.getItem("myMapBookmarks"));
-                if (!bookmarks) bookmarks = [new bookmarkEntry("Overview", null)];
+                if (!bookmarks) bookmarks = [new bookmarkEntry("Overview", options.extent.toJson())];
 
                 // Editable data
                 self.bookmarks = ko.observableArray(bookmarks);
@@ -29,6 +35,7 @@ require(["dojo/ready", "esri/map"],
                 // stores the value the user enters
                 self.current = ko.observable();
 
+                //listen for enter
                 self.addBookmarkEnter = function (model, event) {
                     var keyCode = (event.which ? event.which : event.keyCode);
                     if (keyCode === 13) {
@@ -37,12 +44,20 @@ require(["dojo/ready", "esri/map"],
                     return true;
                 };
 
+                //add a new one
                 self.addBookmark = function () {
                     self.bookmarks.push(new bookmarkEntry(self.current(), map.extent.toJson()));
                     self.current(''); //clear the current value
                     localStorage.setItem("myMapBookmarks", JSON.stringify(self.bookmarks()));
                 };
+
+                //zoom to it
+                self.zoomBookmark = function (item) {
+                    map.setExtent(new esri.geometry.Extent(item.extent));
+                };
+
             }
+            
             ko.applyBindings(new boomarksModelModel());
 
         });
