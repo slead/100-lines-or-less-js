2dc2de6947bcaa0c4c072d12e1ed3c539282ccfe
Stephen Lead <stephen.lead@gmail.com>
2013-03-22 07:14:56 +1100

    new version
    iPhone and iPad support
    temperature trend chart

--- AustralianClimateExplorer/js/climateExplorer.js
+++ AustralianClimateExplorer/js/climateExplorer.js
@@ -4,32 +4,26 @@ dojo.require("esri.dijit.Geocoder");
 dojo.require("dijit.layout.BorderContainer");
 dojo.require("dijit.layout.ContentPane");
 dojo.require("dijit.TooltipDialog");
-var map, dialog, selSym, hcs = [{"chartType":"column", "renderTo": "rainfall",
-	"labels": {"title": "Average monthly rainfall"},
-	"flds": ["rainJan","rainFeb","rainMar","rainApr","rainMay","rainJun","rainJul",
-		"rainAug","rainSep","rainOct","rainNov","rainDec"],
-	"yAxis": {"title": {"text": "millimetres"}},
-	"ttip": {"sfx": " mm/month"}
-},{"chartType":"line", "renderTo": "minTemp",
-	"labels": {"title": "Average minimum temperature"},
-	"flds": ["minJan","minFeb","minMar","minApr","minMay","minJun","minJul",
-		"minAug","minSep","minOct","minNov","minDec"],
-	"yAxis": {"title": {"text": "deg Celsius"}},
-	"ttip": {"sfx": " deg Celsius"}
-},{"chartType":"line", "renderTo": "maxTemp",
-	"labels": {"title": "Average maximum temperature"},
-	"flds": ["maxJan","maxFeb","maxMar","maxApr","maxMay","maxJun","maxJul",
-		"maxAug","maxSep","maxOct","maxNov","maxDec"],
-	"yAxis": {"title": {"text": "deg Celsius"}},
-	"ttip": {"sfx": " deg Celsius"}}];
+var map, dialog, selSym, charts = [{"source": "pt", "chartType":"column", "renderTo": "rainfall",
+	"labels": {"title": "Average monthly rainfall"}, "series": [{"flds": ["rainJan","rainFeb","rainMar",
+	"rainApr","rainMay","rainJun","rainJul","rainAug","rainSep","rainOct","rainNov","rainDec"]}],
+	"yAxis": {"title": {"text": "millimetres"}}, "xAxis": [{"categories": ["Jan","Feb","Mar","Apr",
+	"May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]}],"ttip": {"sfx": " mm/month"}},{"source": "pt",
+	"chartType":"line", "renderTo": "tempRange","labels": {"title": "Average temperature range"},
+	"series": [{"flds": ["minJan","minFeb","minMar","minApr","minMay","minJun","minJul","minAug",
+	"minSep","minOct","minNov","minDec"]},{"flds": ["maxJan","maxFeb","maxMar","maxApr","maxMay",
+	"maxJun","maxJul","maxAug","maxSep","maxOct","maxNov","maxDec"]}],"yAxis": {"title": 
+	{"text": "deg Celsius"}},"xAxis": [{"categories": ["Jan","Feb","Mar","Apr","May","Jun","Jul",
+	"Aug","Sep","Oct","Nov","Dec"]}],"ttip": {"sfx": " deg C"}},{"source": "query","chartType":"line",
+	"renderTo": "maxTemp","ttip": {"sfx": " deg C"},"labels": {"title": "Average maximum temperature"},
+	"yAxis": {"title": {"text": "deg C"}},}];
 var base = $("#source a")[0].dataset.baseurl; //base URL is a dataset attribute
 function init() {
-	map = new esri.Map("map", {center: [133.9, -25.8],zoom: 5,basemap: "gray",
-		maxZoom: 10});
+	map = new esri.Map("map", {center: [133.9, -25.8],zoom: 5,basemap: "gray",maxZoom: 10});
 	selSym = new esri.symbol.SimpleMarkerSymbol().setStyle(
 		esri.symbol.SimpleMarkerSymbol.STYLE_CIRCLE).setColor(new dojo.Color([255,255,0,1]));
 	var ws = new esri.layers.FeatureLayer(
-		"http://54.252.134.128:6080/arcgis/rest/services/climate/WeatherStations/MapServer/0",{
+		"http://services.azuron.com/arcgis/rest/services/weather/WeatherStations/MapServer/0",{
 	    mode: esri.layers.FeatureLayer.MODE_ONDEMAND,outFields: ["*"], opacity: 0.75});
 	dojo.connect(map, "onLoad", function() {map.addLayer(ws);});
 	dojo.connect(map, "onExtentChange", function() {dijit.popup.close();});
@@ -46,8 +40,8 @@ function init() {
 	    dialog = new dijit.TooltipDialog({id: "tooltipDialog"});
 	    dialog.startup();
 	});
-	var geocoder = new esri.dijit.Geocoder({ map: map,
-		arcgisGeocoder: {placeholder: "Find a place", sourceCountry: "AUS"}}, "search");
+	var geocoder = new esri.dijit.Geocoder({ map: map, autoComplete: true,
+		arcgisGeocoder: {placeholder: "Find a place", sourceCountry: "AUS"}},"search");
     geocoder.startup();
 }
 function buildCharts(graphic) { //build charts from this station's values
@@ -58,31 +52,48 @@ function buildCharts(graphic) { //build charts from this station's values
 	map.graphics.clear();
 	var highlight = new esri.Graphic(graphic.geometry, selSym);
 	map.graphics.add(highlight);
-	for (var i = 0; i < hcs.length; i++) {
-		hc = hcs[i];
-	    var attr = graphic.attributes;
-	    var series = new Array();
-	    var flds = hc.flds;
-	    var field = flds[0];
-	    var data = new Array();
-	    data[0] = attr[field];
-	    var numFields = flds.length;
-	    for (var x = 1; x < numFields; x++) {data[x] = attr[flds[x]];}
-	    series[0] = new Object();
-	    series[0].data = data;
-	    var chart = new Highcharts.Chart({
-	        chart: {renderTo: hc.renderTo,defaultSeriesType: hc.chartType,
-	        	marginBottom: 50}, legend: {enabled: false},title: {text: hc.labels.title},
-	        xAxis: [{
-	        	categories: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
-	        }],credits: {enabled: false},
-	        yAxis: hc.yAxis,
-	        tooltip: {formatter: function () {
-	        	return this.point.category+": "+this.point.y+hc.ttip.sfx;}},
-	        series: series
-	    });
-	}
+	for (var i = 0; i < charts.length; i++) {
+		hc = charts[i];
+		var series = new Array();
+		if (hc.source == "pt") {
+			var attr = graphic.attributes;
+			for (var y = 0; y < hc.series.length; y++) {
+				var flds = hc.series[y].flds;
+			    var field = flds[0];
+			    var data = new Array();
+			    data[0] = attr[field];
+			    var numFields = flds.length;
+			    for (var x = 1; x < numFields; x++) {data[x] = attr[flds[x]];}
+			    series[y] = new Object();
+			    series[y].data = data;
+			}
+			drawChart(hc.xAxis, series);
+		} else {
+			var qt = new esri.tasks.QueryTask("http://services.azuron.com/arcgis/rest/services/weather/WeatherStations/MapServer/1");
+			var query = new esri.tasks.Query();
+			query.outFields = ["*"];
+			query.where = "Site = " + graphic.attributes["Site"];
+			qt.execute(query, function(results) {
+				var data = [], cats = [];
+				for (var j = 0; j < results.features.length; j++) {
+					data.push(results.features[j].attributes.maxAvg);
+					cats.push(results.features[j].attributes.year.toString());					
+				}
+				var xAxis = {"categories": cats, labels: {step: Math.round(cats.length/5)}};
+				series[0] = new Object();
+				series[0].data = data;
+				drawChart(xAxis, series);
+			});
+		}}
 }
+function drawChart(xAxis, series) {
+	var chart = new Highcharts.Chart({
+	    chart: {renderTo: hc.renderTo,defaultSeriesType: hc.chartType,
+	    	marginBottom: 50}, legend: {enabled: false},title: {text: hc.labels.title},
+	    xAxis:xAxis,credits: {enabled: false}, yAxis: hc.yAxis,
+	    tooltip: {formatter: function () {
+	    	return this.point.category+": "+this.point.y+hc.ttip.sfx;}},
+	    series: series});}
 function toggleWelcomeDialog() {$("#wd").is(':visible') ? $("#wd").hide() : $("#wd").show();}
 $(document).on("click", "#information", function() {toggleWelcomeDialog()});
 dojo.addOnLoad(init);
